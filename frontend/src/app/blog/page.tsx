a7d3fee334cdf4d008bbbce9044fa6bcf73404d4
ppaauuoo <bcg.channel@gmail.com>
2024-08-03 15:36:20 +0700

    feat!(UI): try using client component

--- frontend/src/app/blog/page.tsx
+++ frontend/src/app/blog/page.tsx
@@ -1,5 +1,6 @@
+'use client'
+
 import { useEffect, useState } from "react";
-import { useClient } from "next/data-client";
 
 type Blog = {
   date: string;
@@ -9,13 +10,12 @@ type Blog = {
 
 export default function Blog() {
   const [data, setData] = useState<Blog[]>([]);
-  useClient(); // Marking this component as a client component
 
   useEffect(() => {
     const fetchData = async () => {
       const url = "http://127.0.0.1:1337/api/blogs";
       const token =
-        "bearer 7e9ef694751918afacef7762dae9078a0b87250ccdc476fa29333c441fdf19401ef75634386edd3876f12fd0acc1ea3542208470544aeac2065ce5be462a6193390ec668d9ac971eba871d8767c22c05076e9364dbb1c7d188bed1ff727136ce83a74a52c1557bb36d37b245b32806c76497cb742f15654c7828dc603be11a64";
+        "bearer 4ad19f838c255ae035ef79504a7a285c19041eccc53c0cc6e4901e7311b442a519ca085647dd73099979162b91a8e6af19f05246a1b7da64b82d6aa69188a448c236cac72f36626d0b8f9647289bbef9db359ea91669f3c1da65eeed646c3e89aba7354ddb793c629bc69bdbc4a46daa8f38b6dce084f1e0707ac88a281bd881";
 
       try {
         const response = await fetch(url, {
@@ -42,6 +42,7 @@ export default function Blog() {
     <main className="prose lg:prose-xl flex min-h-screen flex-col items-center justify-between p-24">
       <div className="p-4">
         <h2 className="text-2xl font-bold">Blog</h2>
+    
         <p>Check out my latest blog posts:</p>
         <ul className="list-disc pl-4">
           {data.map((item: Blog) => (
